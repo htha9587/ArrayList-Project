f967f41963c2d5552f9e02d6b63086de72a129e6
htha9587 <harrison.thacker69@gmail.com>
2015-10-07 13:54:56 -0600

    Part 3
    Added more evolution of words.

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -15,13 +15,20 @@ public class ArrayListController
 	}
 	
 	public void start()
+	/**
+	 * Shows gradual evolution of man.
+	 */
 	{
 		firstWords.add("gggggeoprwiiwepnoewgnreiwoneo");
+		firstWords.add("bbababababababababaab");
 		displayList();
 	}
 	private void displayList()
 	{
 		for(int spot = 0; spot < firstWords.size(); spot++)
+			/**
+			 * Gets list information from our method.
+			 */
 		{
 			arrayDisplay.showList("The contents at " + spot + "are" + firstWords.get(spot));
 		}
