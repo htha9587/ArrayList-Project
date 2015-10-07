b3fd54a9fa71c3b4c9c4cb7181fafd2179c1e3dc
htha9587 <harrison.thacker69@gmail.com>
2015-10-07 13:50:29 -0600

    Loop + Get
    Added get parameter to get array information from our method.

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,17 +1,29 @@
 package list.controller;
 
 import java.util.ArrayList;
+import list.view.ListViewer;
+
 public class ArrayListController 
 {
 	private ArrayList<String> firstWords;
+	private ListViewer arrayDisplay;
 	
 	public ArrayListController()
 	{
 		firstWords = new ArrayList<String>();
+		arrayDisplay = new ListViewer();
 	}
 	
 	public void start()
 	{
 		firstWords.add("gggggeoprwiiwepnoewgnreiwoneo");
+		displayList();
+	}
+	private void displayList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			arrayDisplay.showList("The contents at " + spot + "are" + firstWords.get(spot));
+		}
 	}
 }
